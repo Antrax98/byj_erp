6e52cc5971d21c923c3769d5b40fc54f4bddeb88
AnaysMR <manobazete21@gmail.com>
2025-06-17 23:05:46 -0400

    fix: correccion de comentarios

--- server/src/main/kotlin/dev/byjtech/erp/document_management/infrastructure/exposed/tables/DocumentEditHistoryTable.kt
+++ server/src/main/kotlin/dev/byjtech/erp/document_management/infrastructure/exposed/tables/DocumentEditHistoryTable.kt
@@ -6,9 +6,9 @@ import org.jetbrains.exposed.sql.javatime.datetime
 
 object DocumentEditHistoryTable : UUIDTable("document_edit_history") {
     val documentId = reference("document_id", DocumentsTable) // ID del documento editado
-    val fieldName = text("field_name") // Nombre del campo que fue modificado
-    val oldValue = text("old_value").nullable() // Valor anterior del campo (puede ser nulo)
-    val newValue = text("new_value").nullable() // Nuevo valor del campo (puede ser nulo)
-    val userId = reference("user_id", UsersTable) // Usuario que realizó la modificación
-    val createdAt = datetime("created_at") // Fecha y hora de la modificación
-}
+    val fieldName = text("field_name") // nombre del campo que fue modificado
+    val oldValue = text("old_value").nullable() // valor anterior del campo puede ser nulo
+    val newValue = text("new_value").nullable() // nuevo valor del campo puede ser nulo
+    val userId = reference("user_id", UsersTable) // usuario que hizo la edicion
+    val createdAt = datetime("created_at")
+    }
